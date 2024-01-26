009d7f0896fe0cb28cb762336124532ccdb2de87
Philipp Bohnenstengel <coding@phb23.io>
2024-01-26 23:24:16 +0100

    poc carry item

--- item.gd
+++ item.gd
@@ -1,12 +1,21 @@
 extends RigidBody2D
 var screen_size # Size of the game window.
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	position = randf() * screen_size
-
+	
+	player = get_node("../Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("action"):
+		position = player.global_position
+		z_index = 100
+	else:
+		z_index = 0
