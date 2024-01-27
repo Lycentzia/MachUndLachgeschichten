3b7fca7ea008178190416e445c6fd7d089654e7a
Giulia <ichbineinsommerkind@gmx.de>
2024-01-27 22:11:13 +0100

    Add a lot of more trash

--- level_1.gd
+++ level_1.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-var path : String = ""
 var object_pool : Array = []
 
 var object_bottle = load("res://trash_objects/bottle.tscn")
 var object_greenbottle = load("res://trash_objects/greenbottle.tscn")
+var object_box = load("res://trash_objects/box.tscn")
 var object_can = load("res://trash_objects/can.tscn")
 var object_tunacan = load("res://trash_objects/tunacan.tscn")
 var object_mug = load("res://trash_objects/mug.tscn")
@@ -13,6 +13,11 @@ var object_pizza = load("res://trash_objects/pizza.tscn")
 var object_spray = load("res://trash_objects/spray.tscn")
 var object_coke = load("res://trash_objects/coke.tscn")
 var object_jar = load("res://trash_objects/jar.tscn")
+var object_paperbag = load("res://trash_objects/paperbag.tscn")
+var object_newspaper = load("res://trash_objects/newspaper.tscn")
+var object_milk = load("res://trash_objects/milk.tscn")
+var object_washingagent = load("res://trash_objects/washingagent.tscn")
+var object_waterbottle = load("res://trash_objects/waterbottle.tscn")
 
 var finished = false
 
@@ -20,13 +25,19 @@ func _ready():
 	object_pool.append(object_bottle)
 	object_pool.append(object_greenbottle)
 	object_pool.append(object_can)
+	object_pool.append(object_box)
 	object_pool.append(object_papercup)
 	object_pool.append(object_mug)	
+	object_pool.append(object_milk)	
 	object_pool.append(object_pizza)
 	object_pool.append(object_spray)
 	object_pool.append(object_coke)
 	object_pool.append(object_tunacan)
 	object_pool.append(object_jar)
+	object_pool.append(object_paperbag)
+	object_pool.append(object_newspaper)
+	object_pool.append(object_washingagent)
+	object_pool.append(object_waterbottle)
 	
 	for i in object_pool : 
 		spawn_trash_objects(i)
@@ -35,7 +46,7 @@ func _ready():
 
 func spawn_trash_objects(object):
 	var newobject = object.instantiate()
-	newobject.set_position(Vector2(randf(), randf()) * (get_viewport_rect().size - Vector2(20,20)) + Vector2(10,10))
+	newobject.set_position(Vector2(randf(), randf()) * get_viewport_rect().size)
 	get_parent().add_child(newobject)
 
 func _process(delta):
