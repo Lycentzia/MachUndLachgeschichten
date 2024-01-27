54e6010884e1fa86b600ac2399d0ae00071d4ba1
Philipp Bohnenstengel <coding@phb23.io>
2024-01-27 21:05:16 +0100

    button press with action

--- startButton.gd
+++ startButton.gd
@@ -4,6 +4,10 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _process(delta):
+	if (Input.is_action_just_pressed("action")):
+		_on_pressed()
 
 
 func _on_pressed():
