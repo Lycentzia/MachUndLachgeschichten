31da7cf86b2798cd114f918cddec5506c61cb8c7
Philipp Bohnenstengel <coding@phb23.io>
2024-01-27 19:57:41 +0100

    boss

--- end_screen.gd
+++ end_screen.gd
@@ -1,10 +1,14 @@
-extends Node
+extends Node2D
 
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Camera2D.target = $Player
 	$Camera2D.target_zoom = Vector2(0.2, 0.2)
+	
+	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D_boss.position = screen_size / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
