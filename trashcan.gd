9fd1892955abbac5c2a3ca5b5541c0b1adadfb0e
Philipp Bohnenstengel <coding@phb23.io>
2024-01-27 16:32:31 +0100

    fix happy

--- trashcan.gd
+++ trashcan.gd
@@ -21,7 +21,8 @@ func _process(delta):
 		if player.position.x < position.x && player.position.y < position.y:
 			$AnimatedSprite2D.animation = "top_left"
 	else:
-		$AnimatedSprite2D.animation = "sad"
+		if $AnimatedSprite2D.animation != "happy":
+			$AnimatedSprite2D.animation = "sad"
 
 func _physics_process(delta):
 	for item in get_overlapping_bodies():
