07ae32a5732bb1531ae91e5c2b0da2d5a1ee5d85
Giulia <ichbineinsommerkind@gmx.de>
2024-01-28 13:14:41 +0100

    Some more jokes

--- randomText.gd
+++ randomText.gd
@@ -9,7 +9,11 @@ var arrayWithJokes = [
 	"Warum klaut Robin Hood Deodorant? – Um es unter den Armen zu verteilen",
 	"Wie nennt man den Flur eines Iglus? – Eisdiele.",
 	"Was ist das Lieblingsessen von Models? –Laufsteaks",
-	"Heute wurde jemand mit einer Sellerie mitten auf der Straße erschlagen, war wohl jemand von den Health Angels."
+	"Heute wurde jemand mit einer Sellerie mitten auf der Straße erschlagen, war wohl jemand von den Health Angels.",
+	"Ist Kegeln mit Thomas Anders als mit Dieter Bowlen",
+	"Wenn sich ein Wissenschaftler ein Brötchen belegt, ist es dann wissenschaftlich belegt?",
+	"Der Kopierer rückt die Vorlage nicht mehr raus. Ist wohl ein Raubkopierer.",
+	"Letztens 'nen Typen namens Simon getroffen und er hat mir das Du angeboten. Nenne ihn ab jetzt Dumon."
 ]
 
 # Called when the node enters the scene tree for the first time.
