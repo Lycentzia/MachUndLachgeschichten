ba3c69a3dd6aad22ff7ed68284ceb3fc3ce53bc5
Giulia <ichbineinsommerkind@gmx.de>
2024-01-28 14:40:19 +0100

    Center random text

--- randomText.gd
+++ randomText.gd
@@ -19,7 +19,7 @@ var arrayWithJokes = [
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var randomJoke = arrayWithJokes[randi() % arrayWithJokes.size()]
-	add_text(randomJoke)
+	self.text = "[center]"+ randomJoke + "[/center]"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
